443572c24b9cfb684c726cd5f41da91f573e592d
Ashish-khar <65498683+Ashish-khar@users.noreply.github.com>
2021-12-24 12:18:27 +0530

    Update Ques 4 : Derma&Nurses.py

--- Ques 4 : Derma&Nurses.py
+++ Ques 4 : Derma&Nurses.py
@@ -5,7 +5,6 @@ import numpy as np
 data = pd.read_csv("C:/Users/AK/Downloads/datas.csv")
 
 #droping all others except dermatologists and nurses
-
 data.drop(data[(data["Spec Code"] != "06NRP") &
                (data["Spec Code"] != "06US") &
                (data["Spec Code"] != "01D") &
@@ -23,9 +22,9 @@ data.drop(data[(data["Spec Code"] != "06NRP") &
                .index, inplace= True)
 
 #dropping non FZO products
-
 data= data.drop(data.index[data["Product Code"] == 1])
 
+#Sum number of the prescriptions across the year for each doctor
 new_data = pd.DataFrame(data=data["Physician ID"])
 new2 = pd.DataFrame(data=data[["Jan 2019","Feb 2019","Mar 2019","Apr 2019", "May 2019","Jun 2019","Jul 2019", "Aug 2019","Sep 2019", "Oct 2019","Nov 2019", "Dec 2019"]])
 new2["Total"]=pd.DataFrame(data=new2.sum(axis=1))  
@@ -33,8 +32,10 @@ totf= pd.DataFrame(data=new2["Total"])
 
 final = pd.concat([new_data,totf],axis=1)
 
+#Group them by physician ID
 final2 = final.groupby('Physician ID').sum()
 
+#Deciling
 final2["Decile_rank"] = pd.qcut(final2["Total"], 10, labels=False ,duplicates="drop")
 
 final2
