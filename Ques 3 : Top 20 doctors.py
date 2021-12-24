17e4e6ec7eb07acef7d7af991c1284efd7d76321
Ashish-khar <65498683+Ashish-khar@users.noreply.github.com>
2021-12-24 12:10:55 +0530

    Update Ques 3 : Top 20 doctors.py

--- Ques 3 : Top 20 doctors.py
+++ Ques 3 : Top 20 doctors.py
@@ -5,7 +5,7 @@ import numpy as np
 data = pd.read_csv("C:/Users/AK/Downloads/datas.csv")
 
 
-
+#Summing total prscriptions
 new_data = pd.DataFrame(data=data["Physician ID"])
 
 new2 = pd.DataFrame(data=data[["Jan 2019","Feb 2019","Mar 2019","Apr 2019", "May 2019","Jun 2019","Jul 2019", "Aug 2019","Sep 2019", "Oct 2019","Nov 2019", "Dec 2019"]])
@@ -15,8 +15,9 @@ totf= pd.DataFrame(data=new2["Total"])
 final = pd.concat([new_data,totf],axis=1)
 
 
-
+#Group total by physician id
 final2 = final.groupby('Physician ID').sum()
 
+#sort in descending order and print top 20
 rank=final2.sort_values(by="Total",ascending=False)
 rank.head(20)
